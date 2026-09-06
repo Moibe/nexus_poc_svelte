fa33faeaa21a2b5485d7774897f1af4e6b36df04
Moibe <moi.estrello@gmail.com>
2026-09-05 19:30:14 -0600

    Regresar al icono de lapiz de siempre en Editar (Calibracion)

--- src/lib/components/config/ConfigSheet.svelte
+++ src/lib/components/config/ConfigSheet.svelte
@@ -30,7 +30,6 @@
 	import Clock from '@lucide/svelte/icons/clock';
 	import Trash2 from '@lucide/svelte/icons/trash-2';
 	import Pencil from '@lucide/svelte/icons/pencil';
-	import LapizFirmaIcon from '$lib/components/icons/LapizFirmaIcon.svelte';
 	import Scissors from '@lucide/svelte/icons/scissors';
 	import Archive from '@lucide/svelte/icons/archive';
 	import Sparkles from '@lucide/svelte/icons/sparkles';
@@ -1562,7 +1561,7 @@
 															modalRecorteAbierto = true;
 														}}
 													>
-														<LapizFirmaIcon class="size-4" />
+														<Pencil class="size-4" />
 													</button>
 													<button
 														type="button"
